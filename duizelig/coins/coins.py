1a84a33468bacd931de968818f8600926932598f
99061142 <99061142@mydavinci.nl>
2021-10-03 20:38:13 +0200

    Started the assignment
    Everything done except printing the receipt ( if there is no change )

---
+++ duizelig/coins/coins.py
@@ -0,0 +1,73 @@
+# name of student: Xander Briem
+# number of student: 99061142
+# purpose of program: Print a answer to the user how he can pay the total price with the fastest method
+# function of program: Calculate how the user can pay the price 
+# structure of program: loop through the problem if the user has not paid the total price
+
+
+# Ask how many the user already paid, and change it into a number
+
+paid = input("How many have you paid: ") 
+paid = int( float(paid) * 100 )
+
+# Ask how much the total price is, and change it into a number
+
+toPay = input("Total price: ") 
+toPay = int( float(toPay) * 100 ) 
+
+change = toPay - paid # Calculate the change the user must pay
+
+
+if change > 0: # Check if the user must pay more
+    coinValue = 500 # The amount of cents the user must pay
+
+
+# Ask the user questions if the user have not paid everything
+
+while change > 0 and coinValue > 0: 
+    nrCoins = change // coinValue # Calculate how many coins the user must give back with the coinValue amount
+    
+    # Change the coin value to "euro" or "cents"
+    
+    if coinValue >= 100:
+        coinValue_name = " euro" 
+        coinValue_str = str( coinValue // 100 ) # Change the value of the coin the user must pay, to a string
+
+    else: 
+        coinValue_name = " cents"
+        coinValue_str = str(coinValue) # Change the value of the coin the user must pay, to a string
+
+
+    if nrCoins > 0: # If the user can pay with a coin that is in the coin list
+
+        nrCoins_str = str(nrCoins) # Number of coins the user must pay
+
+        print("return maximal " + nrCoins_str + " coin(s) of " + coinValue_str + coinValue_name ) # Print the amount of coins the user must give back with the specific value
+
+        nrCoinsReturned = input("How many coin(s) of " + coinValue_str + coinValue_name + " did you return? ") # Ask the user how many coins he has paid with the specific value
+        nrCoinsReturned = int(nrCoinsReturned) # Change the input what the user has paid with the specific value to a number
+
+        change -= nrCoinsReturned * coinValue # The change gets decreased with the value the user has paid\
+
+
+    coinArray = [500, 300, 200, 100, 50, 20, 10, 5, 2, 1, 0] # Array with all the coin values
+
+    # Change the value of the coin, to the next coin value in the coin array
+
+    coinValue_position = ( coinArray.index(coinValue) + 1 ) # Get the position of the coin the user did answer + 1
+        
+    coinValue = coinArray[coinValue_position] # Change the coin value with the coinValue_position variable
+
+# Print the answer if the user must pay more, or that the payment is completed
+
+else:
+    change_str = str(change) # Change the change the user must pay, to a string
+
+    
+    # If the user has not paid everything
+    if change:
+        print("Change not returned: " + change_str + " cent(s)") 
+        
+    # If the user paid everything
+    else:
+        print("Payment completed")
