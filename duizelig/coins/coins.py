ca7bd859860850fbd1edbcc71d1a9c85adb64ce0
99061142 <99061142@mydavinci.nl>
2021-10-03 20:53:40 +0200

    Done with the assignment

--- duizelig/coins/coins.py
+++ duizelig/coins/coins.py
@@ -2,8 +2,10 @@
 # number of student: 99061142
 # purpose of program: Print a answer to the user how he can pay the total price with the fastest method
 # function of program: Calculate how the user can pay the price 
-# structure of program: loop through the problem if the user has not paid the total price
+# structure of program: loop through the program if the user has not paid the total price
 
+
+receipt = [] # Array for the receipt
 
 # Ask how many the user already paid, and change it into a number
 
@@ -45,29 +47,38 @@ while change > 0 and coinValue > 0:
         print("return maximal " + nrCoins_str + " coin(s) of " + coinValue_str + coinValue_name ) # Print the amount of coins the user must give back with the specific value
 
         nrCoinsReturned = input("How many coin(s) of " + coinValue_str + coinValue_name + " did you return? ") # Ask the user how many coins he has paid with the specific value
-        nrCoinsReturned = int(nrCoinsReturned) # Change the input what the user has paid with the specific value to a number
+        nrCoinsReturned_int = int(nrCoinsReturned) # Change the input what the user has paid with the specific value to a number
 
-        change -= nrCoinsReturned * coinValue # The change gets decreased with the value the user has paid\
+        change -= ( nrCoinsReturned_int * coinValue ) # The change gets decreased with the value the user has paid\
+
+        # If the coins the user has given back is higher than 0
+        if nrCoinsReturned_int:
+
+            # Add the information to the receipt array
+            receipt_info = nrCoinsReturned + " coin(s) of " + coinValue_str + coinValue_name + " given back" 
+            receipt.append(receipt_info) 
+
 
 
     coinArray = [500, 300, 200, 100, 50, 20, 10, 5, 2, 1, 0] # Array with all the coin values
 
-    # Change the value of the coin, to the next coin value in the coin array
-
     coinValue_position = ( coinArray.index(coinValue) + 1 ) # Get the position of the coin the user did answer + 1
         
     coinValue = coinArray[coinValue_position] # Change the coin value with the coinValue_position variable
+
 
 # Print the answer if the user must pay more, or that the payment is completed
 
 else:
     change_str = str(change) # Change the change the user must pay, to a string
 
-    
     # If the user has not paid everything
     if change:
         print("Change not returned: " + change_str + " cent(s)") 
         
     # If the user paid everything
-    else:
-        print("Payment completed")
+    else:  
+
+        # Print every coin with the amount the user has given back
+        for info in receipt:
+            print(info)
